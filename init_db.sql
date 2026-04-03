b0fcf996450036008d506de09e477241d0f80255
Daria Palianychka <palanickad@gmail.com>
2026-04-03 12:18:06 +0300

    Видозмінена база

--- init_db.sql
+++ init_db.sql
@@ -1,19 +1,35 @@
+DROP TABLE IF EXISTS Application;
 DROP TABLE IF EXISTS Animals;
-DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Type;
+DROP TABLE IF EXISTS Volunteer;
 
-CREATE TABLE Users (
+CREATE TABLE Type (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    surname VARCHAR(100),
-    phone VARCHAR(20) NOT NULL,
-    email VARCHAR(100)
+    breed VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Volunteer (
+    id SERIAL PRIMARY KEY,
+    fullname VARCHAR(200) NOT NULL,
+    phone VARCHAR(20) NOT NULL UNIQUE,
+    email VARCHAR(100) UNIQUE,
+    password VARCHAR(255) NOT NULL, -- Пам'ятаємо про хеш!
+    photo TEXT
 );
 
 CREATE TABLE Animals (
     id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES Users(id),
-    type VARCHAR(50),
-    breed VARCHAR(100),
+    name VARCHAR(100),
+    age INT,
+    id_type INT REFERENCES Type(id),
     description TEXT,
-    status VARCHAR(50)
+    status VARCHAR(50),
+    sex VARCHAR(10),
+    photo TEXT
+);
+
+CREATE TABLE Application (
+    id SERIAL PRIMARY KEY,
+    id_vol INT REFERENCES Volunteer(id) ON DELETE CASCADE,
+    id_ani INT REFERENCES Animals(id) ON DELETE CASCADE
 );
